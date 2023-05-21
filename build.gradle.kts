ee6998333190ec970121ef9c461511de4c10b685
Oliver Eckle <iee1394@gmx.de>
2023-05-21 11:54:19 +0200

    add github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
-    id("org.jetbrains.kotlin.kapt") version "1.8.10"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    id("org.jetbrains.kotlin.kapt") version "1.8.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.21"
     id("org.sonarqube") version "4.0.0.2929"
     id("com.google.cloud.tools.jib") version "3.3.1" apply (false)
     id("io.micronaut.application") version "3.7.6" apply (false)
@@ -51,8 +51,8 @@ subprojects {
         // kotlin
         implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
         implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
-        implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
+        implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.21")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
